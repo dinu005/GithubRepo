93db3ed9cecca1cf60e63d7066314d6d9a25b766
naruto_68 <42301037+dinu005@users.noreply.github.com>
2022-06-12 15:59:43 +0530

    Moving to Fragments

--- app/src/main/java/com/navi/githubrepo/view/MainActivity.kt
+++ app/src/main/java/com/navi/githubrepo/view/MainActivity.kt
@@ -3,7 +3,9 @@ package com.navi.githubrepo.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.navi.githubrepo.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
